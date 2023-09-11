a0de5ea193cb8ec0e3741da890de59033af1e3c7
adampiejak <apiejak@students.kennesaw.edu>
2023-09-11 11:58:02 -0400

    save

---
+++ Python Absolute Beginner/P1M1AdamPiejak.py
@@ -0,0 +1,17 @@
+# Create name check code
+input_test = input("Names of people met in past 24 hours?: ").upper()
+# [ ] get input for input_test variable
+print(input_test)
+# [ ] print "True" message if "John" is in the input or False message if not
+print('JOHN' in input_test)
+
+# [ ] print True message if your name is in the input or False if not
+print('ADAM' in input_test)
+
+# [ ] Challenge: Check if another person's name is in the input - print message
+print('BRIAN' in input_test)
+print("Welcome Brian!")
+
+# [ ] Challenge: Check if a fourth person's name is in the input - print message
+print('EMILY' in input_test) 
+print("Welcome Emily!")
